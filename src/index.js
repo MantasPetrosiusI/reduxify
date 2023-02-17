940d3a7fd9b06c2a85d7eaac95e6ad6adf3e42b8
MantasPetrosiusI <mantas.petrosius@outlook.com>
2023-02-17 14:16:43 +0000

    1412.21

--- src/index.js
+++ src/index.js
@@ -4,8 +4,4 @@ import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
 
-createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-);
+createRoot(document.getElementById("root")).render(<App />);
